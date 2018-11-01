49f5bde88e417b5a52ec8bfcb0100e953ec4a0b9
ReneMurillo <rene.murillo95@hotmail.com>
2018-11-01 17:44:06 -0600

    Primer commit

---
+++ Conversiones/Controllers/ConversionesController.cs
@@ -0,0 +1,27 @@
+﻿using Conversiones.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Conversiones.Controllers
+{
+    public class ConversionesController : Controller
+    {
+        Conversion modelo = new Conversion();
+        // GET: Conversiones
+        public ActionResult Index()
+        {
+            if(Request.HttpMethod == "POST")
+            {
+                var unidadingresada = Request["UnidadIngresada"];
+                var unidadesperada = Request["UnidadEsperada"];
+                var valoruniad = Convert.ToDouble(Request["ValorUnidad"]);
+                ViewBag.Resultado = modelo.RealizarConversion(unidadingresada, valoruniad, unidadesperada);
+                return View();
+            }
+            return View();
+        }
+    }
+}
---
+++ Conversiones/Models/Conversion.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Conversiones.Models
+{
+    public class Conversion
+    {
+        private string UnidadIngresada { get; set; }
+        private double ValorUnidad { get; set; }
+        private string UnidadEsperada { get; set; }
+
+        public string RealizarConversion(string pUnidadIngresada, double pValor, string pUnidadEsperada)
+        {
+            this.UnidadEsperada = pUnidadEsperada;
+            this.ValorUnidad = pValor;
+            this.UnidadIngresada = pUnidadIngresada;
+
+            double resultado = 0;
+            string mensaje = "";
+
+            switch (UnidadIngresada)
+            {
+                case "Centimetros":
+                    if(UnidadEsperada == "Centimetros")
+                    {
+                        resultado = ValorUnidad * 1 ;
+                        mensaje = ValorUnidad + " cm es igual a " + resultado + " cm";
+                    }else if(UnidadEsperada == "Metros")
+                    {
+                        resultado = ValorUnidad / 100;
+                        mensaje = ValorUnidad + " cm es igual a " + resultado + " m";
+                    }
+                    else if (UnidadEsperada == "Kilometros")
+                    {
+                        resultado = (ValorUnidad / 100) / 1000;
+                        mensaje = ValorUnidad + " cm es igual a " + resultado + " km";
+                    }
+                    else if (UnidadEsperada == "Millas")
+                    {
+                        resultado = ((ValorUnidad / 100) / 1000) / 0.621371;
+                        mensaje = ValorUnidad + " cm es igual a " + resultado + " millas";
+                    }
+                    break;
+                case "Metros":
+                    if (UnidadEsperada == "Centimetros")
+                    {
+                        resultado = ValorUnidad * 100;
+                        mensaje = ValorUnidad + " m es igual a " + resultado + " cm";
+                    }
+                    else if (UnidadEsperada == "Metros")
+                    {
+                        resultado = ValorUnidad * 1;
+                        mensaje = ValorUnidad + " m es igual a " + resultado + " m";
+                    }
+                    else if (UnidadEsperada == "Kilometros")
+                    {
+                        resultado = (ValorUnidad / 1000);
+                        mensaje = ValorUnidad + " m es igual a " + resultado + " km";
+                    }
+                    else if (UnidadEsperada == "Millas")
+                    {
+                        resultado = (ValorUnidad / 1000) / 0.621371;
+                        mensaje = ValorUnidad + " m es igual a " + resultado + " millas";
+                    }
+                    break;
+                case "Kilometros":
+                    if (UnidadEsperada == "Centimetros")
+                    {
+                        resultado = (ValorUnidad * 1000) * 100;
+                        mensaje = ValorUnidad + " km es igual a " + resultado + " cm";
+                    }
+                    else if (UnidadEsperada == "Metros")
+                    {
+                        resultado = ValorUnidad * 1000;
+                        mensaje = ValorUnidad + " km es igual a " + resultado + " m";
+                    }
+                    else if (UnidadEsperada == "Kilometros")
+                    {
+                        resultado = ValorUnidad * 1;
+                        mensaje = ValorUnidad + " km es igual a " + resultado + " km";
+                    }
+                    else if (UnidadEsperada == "Millas")
+                    {
+                        resultado = ValorUnidad * 0.621371;
+                        mensaje = ValorUnidad + " km es igual a " + resultado + " millas";
+                    }
+                    break;
+                case "Millas":
+                    if (UnidadEsperada == "Centimetros")
+                    {
+                        resultado = ((ValorUnidad / 0.621371)* 1000) * 100;
+                        mensaje = ValorUnidad + " millas es igual a " + resultado + " cm";
+                    }
+                    else if (UnidadEsperada == "Metros")
+                    {
+                        resultado = (ValorUnidad / 0.621371) * 1000;
+                        mensaje = ValorUnidad + " millas es igual a " + resultado + " m";
+                    }
+                    else if (UnidadEsperada == "Kilometros")
+                    {
+                        resultado = ValorUnidad / 0.621371;
+                        mensaje = ValorUnidad + "millas es igual a " + resultado + " km";
+                    }
+                    else if (UnidadEsperada == "Millas")
+                    {
+                        resultado = ValorUnidad * 1;
+                        mensaje = ValorUnidad + "millas es igual a " + resultado + " millas";
+                    }
+                    break;
+            }
+            return mensaje;
+        }
+    }
+}
